ba53f9cd674a2dcda32e1be535791d604aefd3a3
DimitriSchorochoff <62765707+DimitriSchorochoff@users.noreply.github.com>
2021-11-04 18:19:28 +0100

    Squeleton Layout class

--- classes.py
+++ classes.py
@@ -53,12 +53,45 @@ class Edge:
 
 
 class Attribute:
-    def __init__(self, name, filter_min, filter_max):
+    def __init__(self, name, filter_min, filter_max, isCategorical):
         self.name = name
         self.filter_min = filter_min
         self.filter_max = filter_max
         self.color_list = []
+        self.is_categorical = isCategorical
         self.is_scaling = False
 
 class Layout:
-    pass
+    def __init__(self, name, parameter_lst):
+        self.name = name
+        self.parameter_lst = parameter_lst
+
+    def return_coordinate(self, graph):
+        #Renvoi un tuple ([coord X], [coord Y])
+        pass
+
+class Layout_parameter:
+    def __init__(self, name, category, data_type):
+        self.name = name
+        self.category = category
+        self.data_type = data_type
+        # 0 Layout_parameter_value, 1 Layout_parameter_boolean, 2 Layout_parameter_categorical
+
+
+class Layout_parameter_value(Layout_parameter):
+    def __init__(self, name, category, minimum, maximum, start):
+        super().__init__(name, category, 0)
+        self.minimum = minimum
+        self.maximum = maximum
+        self.start = start
+
+class Layout_parameter_boolean(Layout_parameter):
+    def __init__(self, name, category):
+        super().__init__(name, category, 1)
+        self.coched = False
+
+class Layout_parameter_categorical(Layout_parameter):
+    def __init__(self, name, category, list_choice, start_choice_pos):
+        super().__init__(name, category, 2)
+        self.list_choice = list_choice
+        self.start_choice_pos = start_choice_pos
