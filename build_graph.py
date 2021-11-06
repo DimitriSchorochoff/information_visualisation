cbfe613c0f6bb78c870824d1caa11461fd0ea45a
Thomas Jacquet <90795948+Thomasj17@users.noreply.github.com>
2021-11-06 12:22:00 +0100

    Update build_graph.py

--- build_graph.py
+++ build_graph.py
@@ -1,13 +1,70 @@
-import networkx
+import networkx as nx
 from pyvis.network import Network
+from classes import  *
+
+#TODO
+def load_graph_from_csv(filename_nodes, filename_edges):
+    #add nodes
+    #add edges
+    pass
+
+def draw_graph(graph):
+    #transform a networkx into pyvis
+    pass
+
+def shortest_path(source, dest, graph):
+    #use a etworkx function
+    pass
+
+def draw_graph_layout(layout, graph):
+    #use networkx layouts
+    #one function by layout
+    pass
+
+def add_node(graph, name, color, size, neighbourds):
+    #add a node to networkx graph
+    pass
+
+def add_edge(graph, name, color, size, node1, node2):
+    #add edge to networkx
+    pass
+
+def change_node_color(node, color):
+    pass
+
+def change_node_size(node, size):
+    pass
+
+def change_edge_color(edge, color):
+    pass
+
+def change_edge_size(edge, size):
+    pass
+
+def clustering_coefficient(graph):
+    pass
+
+def betweenness_centrality(graph):
+    pass
+
+def minimum_spanning_tree(graph):
+    pass
+
+def find_communities(graph):
+    pass
+
+def filter(graph):
+    pass
 
 
 
-g = Network('500px', '800px')
-g.add_nodes([1,2,3], value=[10, 100, 400],
-                         title=['I am node 1', 'node 2 here', 'and im node 3'],
-                         x=[21.4, 54.2, 11.2],
-                         y=[100.2, 23.54, 32.1],
-                         label=['NODE 1', 'NODE 2', 'NODE 3'],
-                         color=['#00ff1e', '#162347', '#dd4b39'])
-g.save_graph('nx.html')
+
+
+
+
+nx_graph = nx.complete_graph(200)
+
+nt = Network("1080x", "1820px")
+# populates the nodes and edges data structures
+nt.from_nx(nx_graph)
+nt.show("nx.html")
