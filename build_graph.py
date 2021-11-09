c09a2fb39aa7277ec3a02c250d03d18b55dadaf5
Thomas Jacquet <90795948+Thomasj17@users.noreply.github.com>
2021-11-09 18:01:18 +0100

    Update build_graph.py

--- build_graph.py
+++ build_graph.py
@@ -1,16 +1,50 @@
 import networkx as nx
 from pyvis.network import Network
+import pandas as pd
 from classes import  *
+import matplotlib.pyplot as plt
 
-#TODO
+
 def load_graph_from_csv(filename_nodes, filename_edges):
-    #add nodes
-    #add edges
-    pass
+    df_node = pd.read_csv(filename_nodes, sep='\t')
+    df_edge = pd.read_csv(filename_edges, sep='\t', low_memory=False)
+    nodes_attr = df_node.columns.values.tolist()
+    edges_attr = df_edge.columns.values.tolist()
+    lst_nodes, lst_edges = [], []
+    mem = []
 
-def draw_graph(graph):
-    #transform a networkx into pyvis
-    pass
+    for i in range(len(df_node.index)):
+        lst_nodes.append((int(df_node[nodes_attr[0]][i]), {nodes_attr[j]: df_node[nodes_attr[j]][i] for j in range(1,len(nodes_attr))}))
+
+    special_index = [i for i in range(len(edges_attr))]
+    special_index.remove(3), special_index.remove(4)
+
+    for i in range(len(df_edge.index)):
+        if (df_edge[edges_attr[3]][i], df_edge[edges_attr[4]][i]) not in mem: #Check if the edge is already in the list
+            lst_edges.append((int(df_edge[edges_attr[3]][i]), int(df_edge[edges_attr[4]][i]),{edges_attr[j]: df_edge[edges_attr[j]][i] for j in special_index}))
+            mem.append((df_edge[edges_attr[3]][i], df_edge[edges_attr[4]][i]))
+            mem.append((df_edge[edges_attr[4]][i], df_edge[edges_attr[3]][i]))
+
+        if df_edge[edges_attr[3]][i] not in lst_nodes[:][0]:
+            lst_nodes.append((int(df_edge[edges_attr[3]][i]), {}))
+        if df_edge[edges_attr[4]][i] not in lst_nodes[:][0]:
+            lst_nodes.append((int(df_edge[edges_attr[4]][i]), {}))
+
+
+    G = nx.Graph()
+    G.add_nodes_from([i[0] for i in lst_nodes])
+    G.add_edges_from([i[:2] for i in lst_edges])
+
+    return G
+
+
+def draw_graph(nx_graph):
+    nt_graph = Network('1080px', '1920px')
+    nt_graph.from_nx(nx_graph)
+    nt_graph.show('nx.html')
+    nt_graph.save_graph('nx.html')
+
+
 
 def shortest_path(source, dest, graph):
     #use a etworkx function
@@ -57,14 +91,5 @@ def filter(graph):
     pass
 
 
-
-
-
-
-
-nx_graph = nx.complete_graph(200)
-
-nt = Network("1080x", "1820px")
-# populates the nodes and edges data structures
-nt.from_nx(nx_graph)
-nt.show("nx.html")
+graph = load_graph_from_csv('Data/BIOGRID-PROJECT-glioblastoma_project-GENES.projectindex.csv', 'Data/BIOGRID-PROJECT-glioblastoma_project-INTERACTIONS.tab3.csv')
+draw_graph(graph)
