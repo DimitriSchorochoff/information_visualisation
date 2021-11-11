ce1d5437e3313d1bc5657b2fbb1be6a3d941be5e
Thomas Jacquet <90795948+Thomasj17@users.noreply.github.com>
2021-11-12 00:28:28 +0100

    Update build_graph.py

--- build_graph.py
+++ build_graph.py
@@ -8,88 +8,94 @@ import matplotlib.pyplot as plt
 def load_graph_from_csv(filename_nodes, filename_edges):
     df_node = pd.read_csv(filename_nodes, sep='\t')
     df_edge = pd.read_csv(filename_edges, sep='\t', low_memory=False)
-    nodes_attr = df_node.columns.values.tolist()
-    edges_attr = df_edge.columns.values.tolist()
-    lst_nodes, lst_edges = [], []
-    mem = []
-
-    for i in range(len(df_node.index)):
-        lst_nodes.append((int(df_node[nodes_attr[0]][i]), {nodes_attr[j]: df_node[nodes_attr[j]][i] for j in range(1,len(nodes_attr))}))
-
-    special_index = [i for i in range(len(edges_attr))]
-    special_index.remove(3), special_index.remove(4)
-
-    for i in range(len(df_edge.index)):
-        if (df_edge[edges_attr[3]][i], df_edge[edges_attr[4]][i]) not in mem: #Check if the edge is already in the list
-            lst_edges.append((int(df_edge[edges_attr[3]][i]), int(df_edge[edges_attr[4]][i]),{edges_attr[j]: df_edge[edges_attr[j]][i] for j in special_index}))
-            mem.append((df_edge[edges_attr[3]][i], df_edge[edges_attr[4]][i]))
-            mem.append((df_edge[edges_attr[4]][i], df_edge[edges_attr[3]][i]))
-
-        if df_edge[edges_attr[3]][i] not in lst_nodes[:][0]:
-            lst_nodes.append((int(df_edge[edges_attr[3]][i]), {}))
-        if df_edge[edges_attr[4]][i] not in lst_nodes[:][0]:
-            lst_nodes.append((int(df_edge[edges_attr[4]][i]), {}))
-
 
     G = nx.Graph()
-    G.add_nodes_from([i[0] for i in lst_nodes])
-    G.add_edges_from([i[:2] for i in lst_edges])
-
+    G.add_nodes_from(df_node['#BIOGRID ID'])
+    G.add_edges_from(zip(df_edge['BioGRID ID Interactor A'], df_edge['BioGRID ID Interactor B']), color='black')
     return G
 
 
 def draw_graph(nx_graph):
     nt_graph = Network('1080px', '1920px')
     nt_graph.from_nx(nx_graph)
-    #nt_graph.show('nx.html')
-    nt_graph.save_graph('nx.html')
+    nt_graph.toggle_physics(False)
+    nt_graph.show('nx.html')
+    #nt_graph.save_graph('nx.html')
 
+
+def add_Node(graph, id, label=None, color=None, size=None, neighbourds=None):
+    assert isinstance(graph, nx.Graph)
+    graph.add_node(id, label=label, size=size, color=color)
+    for elem in neighbourds:
+        add_Edge(graph, id, elem)
+
+
+def add_Edge(graph, node1, node2, label=None, color=None, size=None):
+    assert isinstance(graph, nx.Graph)
+    graph.add_edge(node1, node2, label=label, color=color, size=size)
+
+
+def change_node_color(graph, node, color):
+    assert isinstance(graph, nx.Graph)
+    graph.nodes[node]['color'] = color
+
+
+def change_node_size(graph, node, size):
+    assert isinstance(graph, nx.Graph)
+    graph.nodes[node]['size'] = size
+
+
+def change_edge_color(graph, node1, node2, color):
+    assert isinstance(graph, nx.Graph)
+    graph[node1][node2]['color'] = color
+
+
+def change_edge_width(graph, node1, node2, width):
+    assert isinstance(graph, nx.Graph)
+    graph[node1][node2]['width'] = width
 
 
 def shortest_path(source, dest, graph):
-    #use a etworkx function
-    pass
+    path = nx.algorithms.shortest_path(graph, source, dest)
+    idx=0
+    for i in range(len(path)-1):
+        change_node_color(graph, path[idx], 'red')
+        change_node_size(graph,path[idx],100)
+        idx += 1
+        change_edge_color(graph, path[i], path[i+1], 'red')
+        change_edge_width(graph, path[i], path[i+1], 100)
+    change_node_color(graph, path[idx], 'red')
+    change_node_size(graph, path[idx], 100)
+
+
+def minimum_spanning_tree(graph):
+    return nx.algorithms.minimum_spanning_tree(graph)
+
 
 def draw_graph_layout(layout, graph):
-    #use networkx layouts
-    #one function by layout
+    # use networkx layouts
+    # one function by layout
     pass
 
-def add_node(graph, name, color, size, neighbourds):
-    #add a node to networkx graph
-    pass
-
-def add_edge(graph, name, color, size, node1, node2):
-    #add edge to networkx
-    pass
-
-def change_node_color(node, color):
-    pass
-
-def change_node_size(node, size):
-    pass
-
-def change_edge_color(edge, color):
-    pass
-
-def change_edge_size(edge, size):
-    pass
 
 def clustering_coefficient(graph):
     pass
 
+
 def betweenness_centrality(graph):
     pass
 
-def minimum_spanning_tree(graph):
-    pass
 
 def find_communities(graph):
     pass
+
 
 def filter(graph):
     pass
 
 
 graph = load_graph_from_csv('Data/BIOGRID-PROJECT-glioblastoma_project-GENES.projectindex.csv', 'Data/BIOGRID-PROJECT-glioblastoma_project-INTERACTIONS.tab3.csv')
-draw_graph(graph)
+#draw_graph(graph)
+#shortest_path(107140, 108517, graph)
+mst = minimum_spanning_tree(graph)
+draw_graph(mst)
