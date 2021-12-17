6084381098e27e6b5df260d5569b2fd33b4c1dba
DimitriSchorochoff <62765707+DimitriSchorochoff@users.noreply.github.com>
2021-12-17 14:58:33 +0100

    Clean files

--- side_fun.py
+++ side_fun.py
@@ -1,11 +1,230 @@
 import networkx as nx
 from pyvis.network import Network
 import pandas as pd
-import numpy as np
 import community as community_louvain
 import seaborn as sns
 import matplotlib.colors
-from classes import *
+
+# Classes
+class Attribute:
+    def __init__(self, name, type, related_to_node, init_fun):
+        self.name = name
+        self.type = type  # Numerical = 0, Categorical = 1, Algorithm = 2
+        self.related_to_node = related_to_node
+
+        self.is_init = False
+        self.init_fun = init_fun # Graph, Attr -> init all argument properly
+
+    def init_attr(self, graph):
+        if not self.is_init:
+            self.init_fun(graph, self)
+            self.is_init = True
+
+    def reset_attr(self):
+        self.is_init = False
+
+    def filter_graph(self, graph):
+        pass
+
+    def scale_graph(self, graph):
+        pass
+
+
+class Attribute_numerical(Attribute):
+    def __init__(self, name, related_to_node, init_fun, n_decimals=0, min_value=0, max_value=1):
+        super().__init__(name, 0, related_to_node, init_fun)
+        self.absolute_min_value = min_value
+        self.absolute_max_value = max_value
+        self.current_min_value = min_value
+        self.current_max_value = max_value
+        self._filtered_max_value = max_value
+        self._filtered_min_value = min_value
+
+        self.n_decimals = n_decimals
+
+        self.colors = ["#6E85B2", "#5C527F", "#3E2C41", "#261C2C"]  # from https://colorhunt.co/palettes/blue
+        self.values = {} #Node -> value
+        self.scale_with_size = False
+
+    def update_min_max(self):
+        # Set initial value
+        for a, b in self.values.items():
+            self.absolute_min_value = b
+            self.absolute_max_value = b
+            break
+
+        for a, b in self.values.items():
+            if b < self.absolute_min_value:
+                self.absolute_min_value = b
+            elif b > self.absolute_max_value:
+                self.absolute_max_value = b
+
+        self.current_min_value = self.absolute_min_value
+        self.current_max_value = self.absolute_max_value
+
+    def normalize_value(self, value):
+        return (value - self._filtered_min_value) / self._filtered_max_value
+
+    def get_color_index(self, value):
+        n_color = len(self.colors)
+        portion = (self._filtered_max_value - self._filtered_min_value) / n_color
+        for i in range(n_color - 1):
+            if self._filtered_min_value + i * portion <= value <= self._filtered_min_value + (i + 1) * portion:
+                return i
+        return n_color - 1
+
+    def set_filtered_min_max(self):
+        # Compute new min and max value after the filter to have proper normalization
+        self._filtered_min_value = self.absolute_max_value
+        self._filtered_max_value = self.absolute_min_value
+
+        if self.related_to_node:
+            for n, value in self.values.items():
+                if self.current_min_value < value < self.current_max_value:
+                    if value > self._filtered_max_value:
+                        self._filtered_max_value = value
+                    if value < self._filtered_min_value:
+                        self._filtered_min_value = value
+
+        else:
+            for t, v in self.values.keys():
+                if self.current_min_value < self.values[t[0]][t[1]] < self.current_max_value:
+                    if self.values[t[0]][t[1]] > self._filtered_max_value:
+                        self._filtered_max_value = self.values[t[0]][t[1]]
+                    if self.values[t[0]][t[1]] < self._filtered_min_value:
+                        self._filtered_min_value = self.values[v][t[1]]
+
+    def init_attr(self, graph):
+        save_init = self.is_init
+        super().init_attr(graph)
+
+        if not save_init:
+            self.update_min_max()
+
+    def reset_attr(self):
+        super().reset_attr()
+        self.values = {}
+
+        self.absolute_min_value = 0
+        self.absolute_max_value = 1
+        self.current_min_value = 0
+        self.current_max_value = 1
+        self._filtered_max_value = 0
+        self._filtered_min_value = 1
+
+    def filter_graph(self, graph):
+        if self.absolute_min_value == self.current_min_value and self.absolute_max_value == self.current_max_value:
+            return
+
+        # Compute new min and max value after the filter to have proper normalization
+        self._filtered_min_value = self.absolute_max_value
+        self._filtered_max_value = self.absolute_min_value
+
+        if self.related_to_node:
+            nodes_to_remove = []
+            for n in graph.nodes:
+                if (self.values[n] < self.current_min_value) or (self.current_max_value < self.values[n]):
+                    nodes_to_remove.append(n)
+                else:
+                    if self.values[n] > self._filtered_max_value:
+                        self._filtered_max_value = self.values[n]
+                    if self.values[n] < self._filtered_min_value:
+                        self._filtered_min_value = self.values[n]
+
+            graph.remove_nodes_from(nodes_to_remove)
+
+        else:
+            edges_to_remove = []
+            for a, b in graph.edges:
+                if (self.values[a][b] < self.current_min_value) or (self.current_max_value < self.values[a][b]):
+                    edges_to_remove.append((a, b))
+                else:
+                    if self.values[a][b] > self._filtered_max_value:
+                        self._filtered_max_value = self.values[a][b]
+                    elif self.values[a][b] < self._filtered_min_value:
+                        self._filtered_min_value = self.values[a][b]
+
+            graph.remove_edges_from(edges_to_remove)
+
+    def scale_graph(self, graph):
+        if not self.scale_with_size: return
+
+        if self.related_to_node:
+            for n in graph.nodes:
+                node_scaler = self.normalize_value(self.values[n])
+                graph.nodes[n]["size"] = max(1., graph.nodes[n]["size"] * node_scaler)
+
+        else:
+            for a, b in graph.edges:
+                node_scaler = self.normalize_value(self.values[a][b])
+                graph.edges[a][b]["size"] = max(1., graph.edges[a][b]["size"] * node_scaler)
+
+
+class Attribute_categorical(Attribute):
+    def __init__(self, name, related_to_node, init_fun):
+        super().__init__(name, 1, related_to_node, init_fun)
+        self.categories_name = []
+        self.categories_color = []
+        self.categories_to_keep = []
+        self.categories = []  # List of list of node/edge id
+
+    def init_attr(self, graph):
+        save_init = self.is_init
+        super().init_attr(graph)
+
+        if not save_init:
+            for i in range(len(self.categories_name)):
+                self.categories_name[i] = self.categories_name[i] + " (size: {})".format(len(self.categories[i]))
+
+    def reset_attr(self):
+        super().reset_attr()
+        self.categories_name = []
+        self.categories_color = []
+        self.categories_to_keep = []
+        self.categories = []  # List of list of node/edge id
+
+    def filter_graph(self, graph):
+        for i in range(len(self.categories_to_keep)):
+            if not self.categories_to_keep[i]:
+                if self.related_to_node:
+                    graph.remove_nodes_from(self.categories[i])
+                else:
+                    graph.remove_edges_from(self.categories[i])
+
+
+class Layout:
+    def __init__(self, name, parameter_lst):
+        self.name = name
+        self.parameter_lst = parameter_lst
+
+
+class Layout_parameter:
+    def __init__(self, name, data_type):
+        self.name = name
+        self.data_type = data_type
+        # 0 Layout_parameter_value, 1 Layout_parameter_boolean, 2 Layout_parameter_categorical
+
+
+class Layout_parameter_value(Layout_parameter):
+    def __init__(self, name, minimum, maximum, start, step):
+        super().__init__(name, 0)
+        self.minimum = minimum
+        self.maximum = maximum
+        self.start = start
+        self.step = step
+
+
+class Layout_parameter_boolean(Layout_parameter):
+    def __init__(self, name):
+        super().__init__(name, 1)
+        self.coched = False
+
+
+class Layout_parameter_categorical(Layout_parameter):
+    def __init__(self, name, list_choice, start_choice_pos):
+        super().__init__(name, 2)
+        self.list_choice = list_choice
+        self.start_choice_pos = start_choice_pos
 
 LAYOUT_DEFAULT = Layout("Default", [])
 LAYOUT_BARNES = Layout("Barnes Hut", [Layout_parameter_value("Gravity", -100000, -1, -80000, 1000),
@@ -38,7 +257,6 @@ def load_graph_from_csv(filename_nodes, filename_edges):
 
 
 def draw_graph(nx_graph, layout=None):
-    #nt_graph = Network('1080px', '1920px')
     nt_graph = Network(height='100%', width='100%')
     nt_graph.from_nx(nx_graph)
 
@@ -295,29 +513,6 @@ def shortest_path(source, target, graph, method='dijkstra'):
     change_node_color(graph, path[idx], color)
 
 
-def minimum_spanning_tree(graph):
-    return nx.algorithms.minimum_spanning_tree(graph)
-
-
-def clustering_coefficient(graph, nodes=None, avg=False):
-    if avg:
-        return nx.algorithms.average_clustering(graph)
-    # nodes can be an int or a list of int
-    return nx.algorithms.clustering(graph, nodes=nodes)
-
-
-def degree_of_node(graph, nodes=None, avg=False):
-    if avg:
-        return np.mean([i[1] for i in graph.degree()])
-    # nodes can be an int or a list of int
-    return graph.degree(nodes)
-
-
-def betweenness_centrality(graph, k=None, normalized=False, endpoints=False, seed=None):
-    btw_central = nx.betweenness_centrality(graph, k=k, normalized=normalized, endpoints=endpoints, seed=seed)
-    return btw_central
-
-
 def data_node_2_attr_cat(graph, df, attr, node_column, val_column):
     cat_name = {"None": 0}
     name_counter = 1
@@ -353,21 +548,3 @@ def display_data(df):
         if str(df.iloc[0][col]) != '-' and str(df.iloc[0][col]) != 'unknown':
             res += f"<strong>{str(col)}</strong>: {str(df.iloc[0][col])}<br>"
     return res
-
-
-if __name__ == "__main__":
-    graph, df_node, df_edge = load_graph_from_csv('Data\BIOGRID-PROJECT-glioblastoma_project-GENES.projectindex.txt',
-                                                  'Data\BIOGRID-PROJECT-glioblastoma_project-INTERACTIONS.tab3.txt')
-
-    df_chemicals = pd.read_csv('Data\BIOGRID-PROJECT-glioblastoma_project-CHEMICALS.chemtab.txt', sep='\t')
-    df_ptm = pd.read_csv('Data\BIOGRID-PROJECT-glioblastoma_project-PTM.ptmtab.txt', sep='\t')
-    data1 = df_chemicals.loc[df_chemicals['BioGRID Gene ID'] == 106524]
-    # data2 = df_ptm.loc[df_ptm['BioGRID ID'] == 106524]
-    print(df_ptm['BioGRID ID'])
-    # print(display_data(data1))
-    # print(data2)
-
-# SYNONYMS TODO
-Node_colum = ["SYSTEMATIC NAME", "OFFICIAL SYMBOL", "INTERACTION COUNT", "PTM COUNT", "CHEMICAL INTERACTION COUNT",
-              "CATEGORY VALUES", "SUBCATEGORY VALUES"]
-Node_columns_is_num = [False, False, True, True, True, False, False]
